fe06fcf93435c028ff1c93cf253000788922ad0e
Mohamad <khajezade.mohamad@gmail.com>
2020-03-03 17:14:20 +0330

    Initializing the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import os.path
+import re
+
+
+setup(
+    name='shared_directory_manager',
+    description='working with shared directories',
+    packages=find_packages(),
+)

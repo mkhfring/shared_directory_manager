745a3d3ef45c92efa30b280d56afd99bb2bcfb09
Mohamad <khajezade.mohamad@gmail.com>
2020-03-04 11:06:24 +0330

    Implementing shared directory list

--- directory_manager/directory_manager.py
+++ directory_manager/directory_manager.py
@@ -4,40 +4,60 @@ from smb.SMBConnection import SMBConnection
 
 class DirectoryMager():
 
-    def __init__(ip, port, user_name, password):
-        import pudb; pudb.set_trace()  # XXX BREAKPOINT
+    def __init__(self, ip, user_name, password, server_name=None, port=None):
         self.connection = None
         self.server_ip = ip
         self.server_port = port
         self.user_name = user_name
         self.password = password
+        self.server_name = server_name
 
-
-    def list_shared_directories():
+    def list_shared_directories(self):
         raise NotImplementedError
 
-    def connect():
+    def _connect(self):
         raise NotImplementedError
 
-    def upload_file():
+    def upload_file(self):
         raise NotImplementedError
 
-    def get_file():
+    def get_file(self):
         raise NotImplementedError
 
 
 class SMBManager(DirectoryMager):
 
-    def __init__(ip, port, user_name, password):
-        import pudb; pudb.set_trace()  # XXX BREAKPOINT
-        self.connection = SMBConnection()
+    def __init__(self, ip, user_name, password, server_name, port=445):
+        super().__init__(ip, user_name, password, server_name, port)
 
-    def list_shared_directories():
-        pass
+    def list_shared_directories(self):
+        if not self.connection:
+            is_connected = self._connect()
+
+        if is_connected:
+            shares = self.connection.listShares()
+            shared_directories = [
+                share.name for share in shares\
+                if not share.isSpecial and share.name not in [
+                    'NETLOGON',
+                    'SYSVOL'
+                ]
+            ]
+        
+        return shared_directories
 
 
-if __name__ == 'main':
-    smb_manager = SMBManager(1,2,5,7)
-    import pudb; pudb.set_trace()  # XXX BREAKPOINT
-    assert 1 == 1
+    def _connect(self):
+        self.connection = SMBConnection(
+            self.user_name,
+            self.password,
+            self.server_ip,
+            self.server_name,
+            is_direct_tcp=True
+        )
+        is_connected = self.connection.connect(
+            self.server_ip,
+            self.server_port
+        )
+        return is_connected
 
--- tests/test_samba_manager.py
+++ tests/test_samba_manager.py
@@ -1,6 +1,13 @@
-from directory_manager import DirectoryMager
+from directory_manager import SMBManager
 
 
 def test_samba_manager():
-    smb_manager = DirectoryMager(1,2,4,5)
-    assert 1 == 1
+    smb_manager = SMBManager(
+        '192.168.15.135',
+        'kiosktest',
+        'Kss@1$ba',
+        'kioskshare'
+    )
+    shared_directories = smb_manager.list_shared_directories()
+    assert shared_directories is not None
+    assert shared_directories[0] == 'KioskShare'
